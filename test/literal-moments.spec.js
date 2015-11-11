06cd6847c0925a6a5ed81e6304d0dd1d0bf7414e
Michael Demmer <demmer@jut.io>
2015-11-11 13:38:00 -0800

    add first cut scaffolding to parse into moment objects
    Add a simple ASTVisitor object to walk the parse tree and dispatch
    to handler functions for each node type.
    
    Implement the beginnings of a MomentGenerator class that creates a
    moment object out of the parsed tree.
    
    Update the literal-moments test accordingly.

--- test/literal-moments.spec.js
+++ test/literal-moments.spec.js
@@ -1,28 +1,46 @@
 var MomentParser = require('..').MomentParser;
 var _ = require('underscore');
 var expect = require('chai').expect;
+var moment = require('moment');
 
 var parser = new MomentParser();
+
+var now = moment.utc('2015-01-01');
 
 describe('literal moments', function() {
     var tests = {
         'now': {
-            type: 'NowLiteral'
+            ast: {
+                type: 'NowLiteral'
+            },
+
+            moment: now
         },
 
         'today': {
-            type: 'TodayLiteral'
+            ast: {
+                type: 'TodayLiteral'
+            },
+
+            moment: now
         },
 
         '2015-01-01': {
-            type: 'ISODateLiteral',
-            value: "2015-01-01T00:00:00"
+            ast: {
+                type: 'ISODateLiteral',
+                value: "2015-01-01T00:00:00"
+            },
+
+            moment: moment.utc('2015-01-01')
         }
     };
 
-    _.each(tests, function(output, input) {
+    _.each(tests, function(expected, input) {
         it('parses "' + input + '"', function() {
-            expect(parser.parse(input)).deep.equal(output);
+            expect(parser.parse(input)).deep.equal(expected.ast);
+            var m = parser.parseAsMoment(input, {now: now});
+            expect(m._isAMomentObject).is.true;
+            expect(m.toISOString()).deep.equal(expected.moment.toISOString());
         });
     });
 });
