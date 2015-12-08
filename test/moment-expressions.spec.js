86a5415370b0cec284847e3ac02685916d5ae0ae
Will Welch <github@quietplease.com>
2015-12-08 14:45:55 -0700

    added tests to exercise full expression grammar

---
+++ test/moment-expressions.spec.js
@@ -0,0 +1,32 @@
+var MomentParser = require('..');
+var _ = require('underscore');
+var expect = require('chai').expect;
+var moment = require('moment');
+
+var parser = new MomentParser();
+
+var now = moment.utc('2015-01-01T01:02:03.456');
+
+describe('Full moment grammar expression parsing', function() {
+    var tests = {
+        'day 5 of next month':
+            now.clone().startOf('month').add(1, 'month').add(4, 'day'),
+        'month 2 of last year':
+            now.clone().startOf('year').subtract(1, 'year').add(1, 'month'),
+        '2 days after month of 2000-01-01T01:02:03.456':
+            moment.utc('2000-01-01T01:02:03.456').startOf('month').add(2, 'day'),
+        '-m':
+            now.clone().subtract(1, 'minute'),
+        '-M':
+            now.clone().subtract(1, 'month'),
+        '4 months after 2015-06-15':
+            moment.utc('2015-06-15').add(4, 'month'),
+    };
+
+    _.each(tests, function(expected, input) {
+        it('handles "' + input + '"', function() {
+            expect(parser.parseAsMoment(input, {now: now}).isSame(expected)).is.true;
+        });
+    });
+
+});
