00ac5abbf91ea2face2bddd4aa63696d37be0f9b
Michael Demmer <demmer@jut.io>
2015-12-22 16:34:59 -0800

    api: expose generateMoment and generateDuration functions
    To support cases where the caller wants to split out the parse phase
    from the generation phase, add external entry points to generate a
    moment or duration given a parse tree.

--- test/api.spec.js
+++ test/api.spec.js
@@ -10,11 +10,13 @@ var now = moment.utc('2015-01-01T01:02:03.456');
 describe('moment-parser API ', function() {
     it('parses moment strings and returns moments', function() {
         expect(parser.parseMoment("now", {now: now}).isSame(now)).is.true;
+        expect(parser.generateMoment(parser.parse("now"), {now: now}).isSame(now)).is.true;
     });
 
     it('parses duration strings and returns durations', function() {
         var dur = moment.duration(3, 'weeks')
         expect(parser.parseDuration("3 weeks")).deep.equal(dur);
+        expect(parser.generateDuration(parser.parse("3 weeks"))).deep.equal(dur);
     });
 
     it('rejects durations when expecting a moment', function() {
