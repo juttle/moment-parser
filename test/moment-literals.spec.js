767be7824053ffb6d38fcc2eafcce964ae6fb02a
Michael Demmer <demmer@jut.io>
2016-01-04 11:54:48 -0800

    add support for beginning and end
    Instead of throwing an error when generating moments for `beginning` or `end`,
    generate the limits of a javascript Date, i.e. the epoch +/- 100M days.

--- test/moment-literals.spec.js
+++ test/moment-literals.spec.js
@@ -7,6 +7,8 @@ var now = moment.utc('2015-01-01');
 
 describe('literal moment parsing as moments', function() {
     var tests = {
+        'beginning': moment.utc(-100 * 1000000 * 1000 * 24 * 3600),
+        'end': moment.utc(100 * 1000000 * 1000 * 24 * 3600),
         '300': moment.utc(300 * 1000),
         '2015-01-01': moment.utc('2015-01-01'),
         'yesterday': now.clone().startOf('day').subtract(1, 'day'),
@@ -19,21 +21,8 @@ describe('literal moment parsing as moments', function() {
         it('handles "' + input + '"', function() {
             expect(parser.parse(input).valueType).equal('moment');
             var m = parser.parseMoment(input, {now: now});
+            expect(m.toISOString()).equal(expected.toISOString());
             expect(m.isSame(expected)).is.true;
-        });
-    });
-
-    var throws = {
-        'beginning': parser.SyntaxError,
-        'end': parser.SyntaxError
-    };
-
-    _.each(throws, function(expected, input) {
-        it('fails on "' + input + '"', function() {
-            function parseInput() {
-                parser.parseMoment(input);
-            }
-            expect(parseInput).to.throw(expected);
         });
     });
 });
